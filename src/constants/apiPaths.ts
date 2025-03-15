3bc94417104261dc7e20db2e2108f3de8a316a22
Elena_Shokhan <elena_shokhan@epam.com>
2025-03-15 20:21:53 +0200

    task-6

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,7 @@
-const API_PATHS = {
-  product: "https://9bynqhbqf8.execute-api.eu-west-1.amazonaws.com/prod",
+export const API_PATHS = {
+  product: "https://d5l313monl.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://3ox13j6tc0.execute-api.eu-west-1.amazonaws.com/prod",
+  import: "https://9t8btn3uwd.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
-
-export default API_PATHS;
