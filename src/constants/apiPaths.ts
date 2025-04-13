6bdb0aa613cae11484587ceecf304c9ec08589c7
Elena_Shokhan <elena_shokhan@epam.com>
2025-04-13 21:25:37 +0300

    update link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,5 +3,5 @@ export const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://9t8btn3uwd.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://shelenium-cart-api-prod.eu-west-1.elasticbeanstalk.com",
 };
