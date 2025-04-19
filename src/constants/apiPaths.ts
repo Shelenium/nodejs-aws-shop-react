e5d2e88958c1c4237699f936e773d7495125640d
Elena_Shokhan <elena_shokhan@epam.com>
2025-04-19 20:23:46 +0300

    update links

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,6 +2,6 @@ export const API_PATHS = {
   product: "",
   order: "",
   import: "https://9t8btn3uwd.execute-api.eu-west-1.amazonaws.com/prod",
-  bff: "http://shelenium-bff-api-prod.eu-west-1.elasticbeanstalk.com/",
+  bff: "https://d1f1ukztgqqf3t.cloudfront.net",
   cart: "",
 };
